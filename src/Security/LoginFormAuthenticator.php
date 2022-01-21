34791668b63b883a28cec426af03c16615a78fcf
Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-21 19:04:00 +0100

    Redirection vers l'api en cas de succès lors de la connexion

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -48,10 +48,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('api_doc'));
     }
 
     protected function getLoginUrl(Request $request): string
