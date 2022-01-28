85125ae3546ffcf88526b9eb93e07d517600dfbc
Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-28 11:47:34 +0100

    Ajout du début de denormalize qui sera complété en cours cette après-midi

--- src/Serializer/UserDenormalizer.php
+++ src/Serializer/UserDenormalizer.php
@@ -11,7 +11,6 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Security;
 
-
 /**
  * Classe permettant à l'utilisateur de pouvoir changer son mot de passe.
  */
@@ -49,4 +48,16 @@ abstract class UserDenormalizer implements ContextAwareDenormalizerInterface,
         return $reflexionClass->implementsInterface(User::class) && $alreadyCalled === false;
     }
 
+    /**
+     * Ajoutera la valeur true à la clé self::ALREADY_CALLED du context. Ensuite, si l'utilisateur a transmis
+     * un mot de passe dans les données, elle utilisera les services pour hacher le mot de passe (méthode hashPassword()
+     * du passwordHasher, dont le premier paramètre est l'utilisateur connecté récupéré à l'aide de la méthode getUser()
+     * et Security) avant d'invoquer à nouveau la dénormalisation depuis la propriété denormalizer de l'instance courante.
+     */
+    public function denormalize($data, $type, $format = null, array $context = [])
+    {
+        //  Ajoute la valeur true à la clé self::ALREADY_CALLED du context.
+        $context[self::ALREADY_CALLED] = true;
+
+    }
 }
