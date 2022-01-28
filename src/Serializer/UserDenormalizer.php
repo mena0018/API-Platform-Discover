db26db3dffc2b43951ffba3af266acff1c7e47ba
Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-28 10:43:54 +0100

    Ajout du constructeur de la classe UserDenormalizer

--- src/Serializer/UserDenormalizer.php
+++ src/Serializer/UserDenormalizer.php
@@ -6,6 +6,8 @@ namespace App\Serialization\Denormalizer;
 use Symfony\Component\Serializer\Normalizer\ContextAwareDenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Security;
 
 abstract class UserDenormalizer implements ContextAwareDenormalizerInterface,
     DenormalizerAwareInterface
@@ -15,5 +17,9 @@ abstract class UserDenormalizer implements ContextAwareDenormalizerInterface,
     private $passwordHasher;
     private $security;
 
-
+    public function __construct(UserPasswordHasherInterface $passwordHasher, Security $security)
+    {
+        $this->passwordHasher = $passwordHasher;
+        $this->security = $security;
+    }
 }
