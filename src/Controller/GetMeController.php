fb2d0ced2744cdcf3580406e26bf6c7fd5ce9672
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-02-04 14:10:32 +0100

    Création du controller GetMeController

---
+++ src/Controller/GetMeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GetMeController extends AbstractController
+{
+
+}
