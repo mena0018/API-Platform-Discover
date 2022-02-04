0162aad8463fc47467ba91bd26948b45d53a3b2f
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-02-04 15:03:10 +0100

    Récupération et retour de l'utilisateur connecté, ou null sinon, lors de l'invocation du contrôleur

--- src/Controller/GetMeController.php
+++ src/Controller/GetMeController.php
@@ -7,5 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GetMeController extends AbstractController
 {
-
+   public function __invoke(): User
+   {
+       // Récupère l'utilisateur courant depuis les données de session.
+       /** @var User $currentUser */
+       $currentUser = $this->getUser();
+        if ($currentUser === null) {
+            throw $this->createNotFoundException('Aucun utilisateur courant depuis les données de session');
+        }
+        return $currentUser;
+   }
 }
