4fadc9d16943b99033eaf5cf24ebfd85bd08841d
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-02-11 15:37:36 +0100

    Push de fin de séance, test en cours

--- tests/Functional/RatingCreateTest.php
+++ tests/Functional/RatingCreateTest.php
@@ -2,9 +2,20 @@
 
 namespace App\Tests\Functional;
 
+use App\Factory\UserFactory;
 use App\Tests\TestCases\ApiPlatformTestCase;
 
 class RatingCreateTest extends ApiPlatformTestCase
 {
+    public function testAnonymousUserCantCreateNote()
+    {
+        // 1. 'Arrange'
+        UserFactory::createOne();
 
+        // 2. 'Act'
+        self::jsonld_request('POST', '/api/ratings');
+
+        // 3. 'Assert'
+        $this->assertResponseStatusCodeSame(401);
+    }
 }
