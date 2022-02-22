62cbe574e3b0cbf55433a769b6b441235b203fd1
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-02-22 11:16:50 +0100

    Tests en cours - Push de fin de séance

--- tests/Functional/RatingCreateTest.php
+++ tests/Functional/RatingCreateTest.php
@@ -3,10 +3,23 @@
 namespace App\Tests\Functional;
 
 use App\Factory\UserFactory;
+use App\Factory\BookmarkFactory;
+use App\Factory\RatingFactory;
+
 use App\Tests\TestCases\ApiPlatformTestCase;
 
 class RatingCreateTest extends ApiPlatformTestCase
 {
+    protected static function getProperties(): array
+    {
+        return [
+            'id',
+            'user',
+            'bookmark',
+            'value',
+        ];
+    }
+
     public function testAnonymousUserCantCreateNote()
     {
         // 1. 'Arrange'
@@ -17,5 +30,30 @@ class RatingCreateTest extends ApiPlatformTestCase
 
         // 3. 'Assert'
         $this->assertResponseStatusCodeSame(401);
+    }
+
+    public function testAuthenticatedUserCanCreateNote()
+    {
+        $data = [
+            'user' => '/api/users/1',
+            'bookmark' => '/api/bookmarks/1',
+            'value' => 5
+        ];
+
+        // 1. 'Arrange'
+        $user = UserFactory::createOne()->object();
+        BookmarkFactory::createOne()->object();
+        self::$client->loginUser($user);
+
+        // 2. 'Act'
+        $parameters = [
+            'contentType' => 'application/json',
+            'content' => json_encode($data),
+        ];
+        self::jsonld_request('POST', '/api/ratings', $parameters);
+
+        // 3. 'Assert'
+        $json = self::lastJsonResponseWithAsserts(ApiPlatformTestCase::ENTITY, 'Rating');
+        self::assertJsonIsAnItem($json, self::getProperties(), $data);
     }
 }
