543a64cf8e8dd35ea28490a17be987d8571fba5d
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-02-25 15:45:50 +0100

    Push de fin de séance - Exercice 6 en cours

--- tests/Functional/RatingPatchTest.php
+++ tests/Functional/RatingPatchTest.php
@@ -3,12 +3,23 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Factory\BookmarkFactory;
 use App\Factory\RatingFactory;
 use App\Tests\TestCases\ApiPlatformTestCase;
 use App\Factory\UserFactory;
 
 class RatingPatchTest extends ApiPlatformTestCase
 {
+    protected static function getProperties(): array
+    {
+        return [
+            'id',
+            'bookmark',
+            'user',
+            'value',
+        ];
+    }
+
     public function testAnonymousUserCantPatchRating()
     {
         // 1. 'Arrange'
@@ -33,5 +44,25 @@ class RatingPatchTest extends ApiPlatformTestCase
 
         // 3. 'Assert'
         $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testAuthenticatedUserCanPatchOwnData()
+    {
+        // 1. 'Arrange'
+        $dataInit = ['firstname' => 'firstname1', 'lastname' => 'lastname1'];
+        $user = UserFactory::createOne($dataInit)->object();
+        self::$client->loginUser($user);
+
+        // 2. 'Act'
+        $dataPatch = ['lastname' => 'lastname2'];
+        $parameters = [
+            'contentType' => 'application/merge-patch+json',
+            'content' => json_encode($dataPatch),
+        ];
+        self::jsonld_request('PATCH', '/api/ratings/1', $parameters);
+
+        // 3. 'Assert'
+        $json = self::lastJsonResponseWithAsserts(ApiPlatformTestCase::ENTITY, 'Ratings', '/api/ratings/1');
+        self::assertJsonIsAnItem($json, self::getProperties(), array_merge($dataInit, $dataPatch));
     }
 }
