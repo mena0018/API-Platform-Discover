37586d289b1dc5e3567141641dee8e82e5e98c94
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-02-25 15:04:55 +0100

    Ajout du test: 'Un utilisateur anonyme ne peut pas modifier une note' pour PUT et PATCH

--- tests/Functional/RatingPatchTest.php
+++ tests/Functional/RatingPatchTest.php
@@ -3,9 +3,22 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Factory\RatingFactory;
 use App\Tests\TestCases\ApiPlatformTestCase;
+use App\Factory\UserFactory;
 
 class RatingPatchTest extends ApiPlatformTestCase
 {
+    public function testAnonymousUserCantPatchRating()
+    {
+        // 1. 'Arrange'
+        RatingFactory::createOne();
+
+        // 2. 'Act'
+        self::jsonld_request('PATCH', '/api/ratings/1');
+
+        // 3. 'Assert'
+        $this->assertResponseStatusCodeSame(401);
+    }
 
 }
--- tests/Functional/RatingPutTest.php
+++ tests/Functional/RatingPutTest.php
@@ -3,9 +3,21 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Factory\RatingFactory;
+use App\Factory\UserFactory;
 use App\Tests\TestCases\ApiPlatformTestCase;
 
 class RatingPutTest extends ApiPlatformTestCase
 {
+    public function testAnonymousUserCantPutRating()
+    {
+        // 1. 'Arrange'
+        RatingFactory::createOne();
 
+        // 2. 'Act'
+        self::jsonld_request('PUT', '/api/ratings/1');
+
+        // 3. 'Assert'
+        $this->assertResponseStatusCodeSame(401);
+    }
 }
