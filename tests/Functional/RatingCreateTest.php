2bcb011afbc72a6208b1268eefc78a70824bf0eb
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-02-25 13:59:41 +0100

    Ajout du test: 'Un utilisateur authentifié ne peut pas créer une note pour un bookmark pour lequel il a déjà mis une note'

--- tests/Functional/RatingCreateTest.php
+++ tests/Functional/RatingCreateTest.php
@@ -54,4 +54,29 @@ class RatingCreateTest extends ApiPlatformTestCase
         $json = self::lastJsonResponseWithAsserts(ApiPlatformTestCase::ENTITY, 'Rating', '/api/ratings/1');
         self::assertJsonIsAnItem($json, self::getProperties(), $data);
     }
+
+    public function testAuthenticatedUserCantCreateNoteForBookmark()
+    {
+        $this->testAuthenticatedUserCanCreateNote();
+        $data = [
+            'bookmark' => '/api/bookmarks/1',
+            'user' => '/api/users/1',
+            'value' => 5
+        ];
+
+        // 1. 'Arrange'
+        $user = UserFactory::createOne()->object();
+        BookmarkFactory::createOne()->object();
+        self::$client->loginUser($user);
+
+        // 2. 'Act'
+        $parameters = [
+            'contentType' => 'application/ld+json',
+            'content' => json_encode($data),
+        ];
+        self::jsonld_request('POST', '/api/ratings', $parameters);
+
+        // 3. 'Assert'
+        $this->assertResponseStatusCodeSame(422);
+     }
 }
