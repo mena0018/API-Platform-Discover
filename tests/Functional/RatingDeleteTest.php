a8ba7bd14f8ba5cc6b7769ca80457ef51a2c054a
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-03-01 10:42:37 +0100

    Mise en place de la classe de test RatingDeletetest.php

---
+++ tests/Functional/RatingDeleteTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use App\Factory\UserFactory;
+use App\Tests\TestCases\ApiPlatformTestCase;
+
+
+class RatingDeleteTest extends ApiPlatformTestCase
+{
+
+}
