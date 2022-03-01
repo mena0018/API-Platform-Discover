002b979cd922dfc154d40cd78c2210276ac106bb
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-03-01 11:08:06 +0100

    Exercice 7 - Ajout du test : Un utilisateur anonyme ne peut pas supprimer une note

--- tests/Functional/RatingDeleteTest.php
+++ tests/Functional/RatingDeleteTest.php
@@ -3,11 +3,24 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
+use App\Factory\RatingFactory;
 use App\Factory\UserFactory;
 use App\Tests\TestCases\ApiPlatformTestCase;
 
 
 class RatingDeleteTest extends ApiPlatformTestCase
 {
+    public function testAnonymousUserCantDeleteMark()
+    {
+        // 1. 'Arrange'
+        UserFactory::createOne()->object();
+        RatingFactory::createOne()->object();
+
+        // 2. 'Act'
+        self::jsonld_request('DELETE', '/api/ratings/1');
+
+        // 3. 'Assert'
+        $this->assertResponseStatusCodeSame(401);
+    }
 
 }
