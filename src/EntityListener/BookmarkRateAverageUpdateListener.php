614ca80626f0c79eb63332fae24dd6d3bb110660
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-03-11 15:24:43 +0100

    Création de l'écouteur BookmarkRateAverageUpdateListener

--- src/EntityListener/BookmarkRateAverageUpdateListener.php
+++ src/EntityListener/BookmarkRateAverageUpdateListener.php
@@ -1,2 +1,37 @@
 <?php
+
 declare(strict_types=1);
+
+namespace App\EntityListener;
+
+use App\Entity\Bookmark;
+use App\Entity\Rating;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class BookmarkRateAverageUpdateListener
+{
+    private EntityManagerInterface $entityManagerInterface;
+
+    public function __construct(EntityManagerInterface $entityManagerInterface)
+    {
+        $this->entityManagerInterface = $entityManagerInterface;
+    }
+
+    public function updateRateAverage(Rating $rating)
+    {
+        $repo = $this->entityManagerInterface->getRepository(Bookmark::class);
+        $repo->updateRateAverage($rating->getBookmark()->getId());
+    }
+
+    public function postPersist(Rating $rating){
+
+    }
+    public function postRemove(Rating $rating){
+
+    }
+    public function postUpdate(Rating $rating){
+
+    }
+
+}
